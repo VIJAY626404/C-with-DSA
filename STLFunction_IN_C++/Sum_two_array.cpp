3dcacaa4810771e4f57fbf97a8b798b4925e7eff
Vijay Kumar Shah <97323848+VIJAY626404@users.noreply.github.com>
2022-12-29 10:04:18 +0530

    Add files via upload

---
+++ STLFunction_IN_C++/Sum_two_array.cpp
@@ -0,0 +1,53 @@
+// implementation of sum of two array
+#include <bits/stdc++.h> 
+vector<int>reverse(vector<int>v){
+    int s = 0;
+    int e = v.size()-1;
+    while(s<=e){
+        swap(v[s++],v[e--]);
+    }
+    return v;
+}
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	// Write your code here.
+    int i = n-1;
+    int j = m-1;
+    int carry =0; 
+    vector<int>ans;
+    while(i>=0 && j>=0){
+        int value1 = a[i];
+        int value2 = b[j];
+        int sum = value1+ value2+ carry ;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+//     first case
+    while(i>=0){
+        int sum = a[i]+carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        i--;
+    }
+//      second case
+    while(j>=0){
+        int sum = b[j]+carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+        j--;
+    }
+//      third case
+    while(carry!= 0){
+        int sum = carry;
+        carry = sum/10;
+        sum = sum%10;
+        ans.push_back(sum);
+    }
+//     print(ans);
+    return reverse(ans);
+   
+}
